f77b642baaaf2d06e8e7a73e17de25098c5cb087
MohammedDharar <87014863+MohammedDharar@users.noreply.github.com>
2021-07-06 17:49:39 +0530

    Update ConsumerController.java

--- src/main/java/com/cognizant/controller/ConsumerController.java
+++ src/main/java/com/cognizant/controller/ConsumerController.java
@@ -29,7 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequestMapping("/consumer")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="*")
 public class ConsumerController {
 	
 	private ConsumerMicroservicesImpl consumerMicroservicesImpl;
